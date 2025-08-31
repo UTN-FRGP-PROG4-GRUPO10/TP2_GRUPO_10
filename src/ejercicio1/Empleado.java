ff70f9927ec7d509abbf100066e94ff3d7be5ab6
Tomás Martínez <tomas.emart@gmail.com>
2025-08-31 13:27:05 -0300

    primer commit, crear proyecto y copiar clases Empleado y Persona

---
+++ src/ejercicio1/Empleado.java
@@ -0,0 +1,59 @@
+package ejercicio1;
+
+import java.time.LocalDate;
+
+public class Empleado extends Persona {
+
+	private final int legajo;
+	private String puesto;
+	private static int contadorLegajo = 1000;
+	
+	public Empleado() {
+		super();
+		this.legajo = contadorLegajo;
+		contadorLegajo++;
+		this.puesto = "sin puesto";
+	}
+
+	public Empleado(
+				String dni, 
+				String nombre, 
+				String apellido, 
+				LocalDate fechaNacimiento,
+            	String genero, String direccion, String telefono, String email,
+            	String puesto)
+	{
+		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		this.legajo = contadorLegajo++;
+		this.puesto = puesto;
+	}
+
+	public static int devuelveProximoLegajo() {
+	    return contadorLegajo;
+	}
+
+
+	
+	@Override
+	public String toString() {
+	    return "Empleado{" +
+	           "legajo= " + legajo +
+	           ", puesto='" + puesto + '\'' +
+	           ", " + super.toString() +
+	           '}';
+	}
+
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	 
+}
